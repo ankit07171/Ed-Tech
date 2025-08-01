16490365fb7d5cdced5109db3de8b279773d04af
akt.07171@gmail.com <ankit11042005@gmail.com>
2025-08-01 23:39:54 +0530

    meet and ui update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { ThemeProvider, useTheme } from './context/ThemeContext.jsx';
-import { SocketProvider } from "./context/SocketContext.jsx";
+import { ThemeProvider, useTheme } from './context/ThemeContext.jsx'; 
 function ThemedApp() {
   const { isDark } = useTheme();
   return (
     <div className={isDark ? 'dark' : ''}>
-      <SocketProvider>
       <App />
-      </SocketProvider>
     </div>
   );
 }
